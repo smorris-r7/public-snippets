d5d36ddc9f571f41eb987a550e9b7a253e633c84
Sam Morris <sfmorris@gmail.com>
2015-12-17 18:25:17 -0600

    use logging module

--- rank.py
+++ rank.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 import sys
 import math
 
@@ -10,17 +11,17 @@ def evaluate(letters):
     The latest perm gets a rank of factorial(len(word)). If the word contains duplicate letters, return the lowest
     possible rank. Execution time must beat 500 ms and memory used must stay under 1 GB.
     """
-    if sys.flags.debug:
-        print("letters: {0}".format(letters))
-        print("remaining: {0}".format(remaining))
+    logging.debug("letters: {0}".format(letters))
+    logging.debug("remaining: {0}".format(remaining))
     for letter in letters:
-        if sys.flags.debug:
-            print("letter: {0}".format(letter))
-            #print("next_branches (len(remaining)-1)!: {0}".format(math.factorial(len(remaining)-1)))
-            #print("index_in_remaining: {0}".format(remaining.index(letter)))
+        logging.debug("letter: {0}".format(letter))
+        #logging.debug("next_branches (len(remaining)-1)!: {0}".format(math.factorial(len(remaining)-1)))
+        #logging.debug("index_in_remaining: {0}".format(remaining.index(letter)))
     return 0
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--word", help="a 25 letter or less word to evaluate", required=True)
     args = parser.parse_args()
